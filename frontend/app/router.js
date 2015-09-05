c194d55cdff98ea287055674f4d4fce266511b57
Adel Qalieh <aqalieh95@gmail.com>
2015-09-05 03:56:44 -0400

    Rename task to habit

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -8,7 +8,7 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.route('login');
   this.route('dashboard');
-  this.route('task-details', { path: '/task/:task_id' });
+  this.route('habit-details', { path: '/habit/:habit_id' });
   this.route('checkin');
 });
 
