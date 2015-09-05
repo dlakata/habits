b3ec6b687b523790b03490831496184ae8b54bac
Adel Qalieh <aqalieh95@gmail.com>
2015-09-05 04:18:52 -0400

    Add habits routing and rename from habit-details

---
+++ frontend/app/routes/habit.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function() {
+    return {
+      title: "Go to the gym",
+      description: "Make sure to work out at the gym for at least 30 minutes a day",
+    };
+  }
+});
