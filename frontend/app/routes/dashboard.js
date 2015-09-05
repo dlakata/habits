10b052b96a7fa171f5c28d51ed231ff455eb4179
David Lakata <davidlakata@gmail.com>
2015-09-05 15:43:50 -0400

    adds settings and new habit buttons

--- frontend/app/routes/dashboard.js
+++ frontend/app/routes/dashboard.js
@@ -3,11 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function () {
     return [{
+      id: 1,
       title: "Go to the gym",
       description: "Make sure to work out at the gym for at least 30 minutes a day",
       time: 30,
       goal: 60
     }, {
+      id: 2,
       title: "Brush teeth",
       description: "Brush teeth twice a day or after every meal",
       time: 25,
