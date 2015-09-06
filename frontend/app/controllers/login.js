f984223a7af6db61df41402291d2b38833854dba
Adel Qalieh <aqalieh95@gmail.com>
2015-09-06 04:40:30 -0400

    Add visual feedback for login form

--- frontend/app/controllers/login.js
+++ frontend/app/controllers/login.js
@@ -1,21 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  loginFailed: false,
+  signupFailed: false,
+  isProcessing: false,
+
   actions: {
     signUp: function() {
+      this.setProperties({
+        signupFailed: false,
+        isProcessing: true
+      });
+
       $.post('http://localhost:5000/user', {
         first_name: this.get('firstName'),
         last_name: this.get('lastName'),
         email: this.get('sEmail'),
         password: this.get('sPassword')
       }).then(function() {
+        this.set('isProcessing', false);
         document.location = '/dashboard';
-      }, function() {
-        this.set("loginFailed", true);
+      }.bind(this), function() {
+        this.set('isProcessing', false);
+        this.set("signupFailed", true);
       }.bind(this));
     },
 
     authenticate: function() {
+      this.set('loginFailed', false);
       var credentials = {
         identification: this.get('email'),
         password: this.get('password')
@@ -23,6 +35,9 @@ export default Ember.Controller.extend({
       var authenticator = 'simple-auth-authenticator:token';
 
       this.get('session').authenticate(authenticator, credentials);
+      if ($.isEmptyObject(this.get('session').get('content').secure)) {
+        this.set('loginFailed', true);
+      }
     }
   }
 });
