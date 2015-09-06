d23bd2314d084aa9b1c9688d01ddaaebcf93416e
Adel Qalieh <aqalieh95@gmail.com>
2015-09-06 05:49:38 -0400

    Start mocking user models/records

--- frontend/app/adapters/habit.js
+++ frontend/app/adapters/habit.js
@@ -2,7 +2,7 @@ import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
   buildURL: function(type, id, record) {
-    var url = this.host + '/user/' + 1 + '/habits';
+    var url = this.host + '/user/' + this.store.find('user') + '/habits';
     if (id !== null) {
       url += '/' + id;
     }
